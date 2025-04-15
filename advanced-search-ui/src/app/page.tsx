f8b7f16bbc25832618a5c609817bc41d084e303c
csj <csjacsj@github.com>
2025-04-15 19:20:21 +0800

    some works on ui

--- advanced-search-ui/src/app/page.tsx
+++ advanced-search-ui/src/app/page.tsx
@@ -1,94 +1,31 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/ui/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/ui/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div>Project path:<input style={{ minWidth: 480 }}></input></div>
+        <div>Main search:
+          <input style={{ minWidth: 130 }}></input>
+          <span style={{ marginLeft: 12 }}></span>
+        Around <input style={{ width: 32, marginLeft: 2 }}></input> Rows
+        <Image src={'/ui/right-svgrepo-com.svg'} alt={'->'} width={18} height={18} style={{ verticalAlign: 'bottom', marginLeft: 8 }}></Image>
+        <input></input>
+        </div>
+        <div>
+          <Image src={'/ui/right-svgrepo-com.svg'} alt={'->'} width={18} height={18} style={{ verticalAlign: 'bottom', marginLeft: 35 }}></Image>
+          <input></input>
+          <Image src={'/ui/right-svgrepo-com.svg'} alt={'->'} width={18} height={18} style={{ verticalAlign: 'bottom', marginLeft: 8 }}></Image>
+          <input></input>
+          <Image src={'/ui/right-svgrepo-com.svg'} alt={'->'} width={18} height={18} style={{ verticalAlign: 'bottom', marginLeft: 8 }}></Image>
+          <input></input>
+          <button style={{ marginLeft: 8, paddingLeft: 4, paddingRight: 4 }}>Search</button>
+          <div className={styles.loadingSpin} style={{ display: "inline-block", marginLeft: 4, marginTop: 0, verticalAlign: 'bottom' }}></div>
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/ui/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/ui/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/ui/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div>Advanced Search UI</div>
       </footer>
     </div>
   );
